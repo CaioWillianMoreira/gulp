20371ead297ef10c7c50bd67e538b34992bcbcc4
Caio Willian <cwillian40@hotmail.com>
2020-08-30 22:24:23 -0300

    [#02 - 24] Minificando HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var html = require("gulp-htmlmin");
+
+// html
+gulp.task("html", function() {
+  return gulp.src("./src/index.html")
+  .pipe(html({collapseWhitespace: true}))
+  .pipe(gulp.dest("./dist/"));
+});
 
 gulp.task("default", function() {
   return;
