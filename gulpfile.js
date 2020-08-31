0f8ce6ba6dc73fc2eec7fde9ec40226c3668333b
Caio Willian <cwillian40@hotmail.com>
2020-08-30 23:16:06 -0300

    [#02 - 25] Minificando HTML e ajustando versões das dependências

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require("gulp");
-var html = require("gulp-htmlmin");
+var gulp = require('gulp');
+var html = require('gulp-htmlmin');
 
 // html
-gulp.task("html", function() {
-  return gulp.src("./src/index.html")
+gulp.task('html', function() {
+  return gulp.src('./src/index.html')
   .pipe(html({collapseWhitespace: true}))
-  .pipe(gulp.dest("./dist/"));
+  .pipe(gulp.dest('./dest/'));
 });
 
-gulp.task("default", function() {
-  return;
+gulp.task('default', ['html'], function() {
+  gulp.watch('./src/index.html', ['html']);
 });
