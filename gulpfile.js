2efd800d7e3b9ae97cf4ce1fa09be1fbc61e6f79
Caio Willian <cwillian40@hotmail.com>
2020-09-02 22:32:02 -0300

    [#02 - 31] Browser sync pt2

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,26 @@ var gulp = require('gulp');
 var html = require('gulp-htmlmin');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
+
+// browser sync
+gulp.task('BS', ['html', 'sass'],function() {
+  browserSync.init({
+    server: {
+      baseDir: './dest/'
+    }
+  })
+  gulp.watch('./src/index.html', ['html']);
+  gulp.watch('./src/scss/style.scss', ['sass']);
+});
 
 // sass
 gulp.task('sass', function() {
   return gulp.src('./src/scss/style.scss')
   .pipe(sass({outputStyle: 'compressed'}))
   .on('error', notify.onError('Error: <%= error.message %>'))
-  .pipe(gulp.dest('./css/'));
+  .pipe(gulp.dest('./css/'))
+  .pipe(browserSync.stream());
 });
 
 // html
@@ -16,10 +29,8 @@ gulp.task('html', function() {
   return gulp.src('./src/index.html')
   .pipe(html({collapseWhitespace: true}))
   .on('error', notify.onError('Error: <%= error.message %>'))
-  .pipe(gulp.dest('./dest/'));
+  .pipe(gulp.dest('./dest/'))
+  .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['html', 'sass'], function() {
-  gulp.watch('./src/index.html', ['html']);
-  gulp.watch('./src/scss/style.scss', ['sass']);
-});
+gulp.task('default', ['BS']);
